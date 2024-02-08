04f57961afaa1cafe9f56c6a4d907861ec963be9
Habeeba00 <habibamohamed2222009u665@gmail.com>
2024-02-08 22:31:28 +0200

    third commit

--- tabled/settings.py
+++ tabled/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o27(eg90%6w83cc-l%k@+300klx^j9^953-j^avnvn9+3uouv$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
