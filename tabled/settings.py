bad7448c5f310a09fdcbbd02a264bbcab340b9c9
Habeeba00 <habibamohamed2222009u665@gmail.com>
2024-02-09 16:38:59 +0200

    seven commit

--- tabled/settings.py
+++ tabled/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o27(eg90%6w83cc-l%k@+300klx^j9^953-j^avnvn9+3uouv$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -117,6 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+
 STATIC_ROOT =os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
